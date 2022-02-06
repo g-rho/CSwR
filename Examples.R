438ec25f04cf3038922e281e2db0e5838e0b0fd2
g-rho <gero.szepannek@web.de>
2022-02-06 11:48:23 +0100

    Files updated (latest versions tested) -- pKg creditmodel added and woeR removed.

--- Examples.R
+++ Examples.R
@@ -1,6 +1,41 @@
-# Accompanying code to the 2019 version of paper.
-# A container with all packages in order to run the examples2019.R and the supplementary_code2019.R 
-# is available at docker hub and can be accessed via: docker run -p 8787:8787 -e PASSWORD=pwd grho/cswr.
+# > sessionInfo()
+# R version 4.1.2 (2021-11-01)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19042)
+# 
+# Matrix products: default
+# 
+# locale:
+# [1] LC_COLLATE=German_Germany.1252  LC_CTYPE=German_Germany.1252    LC_MONETARY=German_Germany.1252 LC_NUMERIC=C                   
+# [5] LC_TIME=German_Germany.1252    
+# 
+# attached base packages:
+# [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+# [1] scoringTools_0.1.2          smbinning_0.9               Formula_1.2-4               partykit_1.2-15             mvtnorm_1.1-3              
+# [6] libcoin_1.0-9               sqldf_0.4-11                RSQLite_2.2.9               gsubfn_0.7                  proto_1.0.0                
+# [11] pROC_1.18.0                 scorecardModelUtils_0.0.1.0 creditmodel_1.3.0           ClustVarLV_2.0.1            ape_5.6                    
+# [16] ClustOfVar_1.1              corrplot_0.92               riskr_1.0                   creditR_0.1.0               MLmetrics_1.1.1            
+# [21] cluster_2.1.2               woeBinning_0.1.6            magrittr_2.0.1              klaR_0.6-15                 MASS_7.3-54                
+# [26] scorecard_0.3.6            
+# 
+# loaded via a namespace (and not attached):
+# [1] colorspace_2.0-2    ellipsis_0.3.2      class_7.3-19        PCAmixdata_3.1      rstudioapi_0.13     proxy_0.4-26        farver_2.1.0       
+# [8] bit64_4.0.5         fansi_0.5.0         lubridate_1.8.0     codetools_0.2-18    splines_4.1.2       doParallel_1.0.16   cachem_1.0.6       
+# [15] knitr_1.37          jsonlite_1.7.2      shiny_1.7.1         compiler_4.1.2      assertthat_0.2.1    Matrix_1.4-0        fastmap_1.1.0      
+# [22] cli_3.1.0           later_1.3.0         htmltools_0.5.2     tools_4.1.2         gtable_0.3.0        glue_1.6.0          reshape2_1.4.4     
+# [29] dplyr_1.0.7         Rcpp_1.0.7          carData_3.0-4       vctrs_0.3.8         nlme_3.1-153        iterators_1.0.13    inum_1.0-4         
+# [36] xfun_0.29           stringr_1.4.0       openxlsx_4.2.5      mime_0.12           miniUI_0.1.1.1      lifecycle_1.0.1     scales_1.1.1       
+# [43] hms_1.1.1           promises_1.2.0.1    parallel_4.1.2      yaml_2.2.1          memoise_2.0.1       gridExtra_2.3       ggplot2_3.3.5      
+# [50] labelled_2.9.0      rpart_4.1-15        stringi_1.7.6       highr_0.9           foreach_1.5.1       randomForest_4.6-14 e1071_1.7-9        
+# [57] zip_2.2.0           shape_1.4.6         chron_2.3-56        rlang_0.4.12        pkgconfig_2.0.3     evaluate_0.14       lattice_0.20-45    
+# [64] ROCR_1.0-11         purrr_0.3.4         labeling_0.4.2      bit_4.0.4           tidyselect_1.1.1    gbm_2.1.8           plyr_1.8.6         
+# [71] R6_2.5.1            generics_0.1.1      combinat_0.0-8      DBI_1.1.2           pillar_1.6.4        haven_2.4.3         survival_3.2-13    
+# [78] abind_1.4-5         tibble_3.1.6        crayon_1.4.2        car_3.0-12          questionr_0.7.5     xgboost_1.5.0.2     utf8_1.2.2         
+# [85] rmarkdown_2.11      data.table_1.14.2   blob_1.2.2          forcats_0.5.1       digest_0.6.29       xtable_1.8-4        tidyr_1.1.4        
+# [92] httpuv_1.6.5        munsell_0.5.0       glmnet_4.1-3        tcltk_4.1.2    
+
 
 ### example 1: load data
 library(scorecard)
@@ -8,7 +43,7 @@ data(germancredit)
 # transform character variable purpose into factor
 germancredit$purpose <- as.factor(germancredit$purpose)
 
-tv <- split_df(germancredit, y = "creditability", ratio = c(0.7,0.3) , seed = 42, 
+tv <- split_df(germancredit, y = "creditability", ratio = c(0.7, 0.3), seed = 42, 
                no_dfs = 2, name_dfs = c("train", "valid"))
 train <- tv$train
 valid <- tv$valid
@@ -18,25 +53,30 @@ train2 <- train; valid2 <- valid
 train2$creditability <- as.integer(train2$creditability == "good")
 valid2$creditability <- as.integer(valid2$creditability == "good")
 
+# the package creditmodel does not support variables of type Factor
+train3 <- as.data.frame(train2)
+valid3 <- as.data.frame(valid2)
+for (j in which(sapply(train3[,-21], is.factor))) {
+  train3[,j] <- as.character(train3[,j])
+  valid3[,j] <- as.character(valid3[,j])
+}
 
 
 ### example 2: automatic binning
 library(scorecard)
 bins         <- woebin(train, y = "creditability", method = "tree")
 
-# binning results for variable purpose
+# binning results table for variable purpose
 options(digits = 3)
-bins$purpose[,c(2,3,4,5,6,7,8,10)]
+bins$purpose[,c(2,4,5,6,7,8)]
 
-# vizualize bins for variable purpose
+# visualize bins for variable purpose
 woebin_plot(bins, x = "purpose", line_value = "woe")
-
 
 
 ### example 3: apply binning to data
 train_bins <- woebin_ply(train, bins, to = "bin")
 valid_bins <- woebin_ply(valid, bins, to = "bin")
-
 
 
 ### example 4: computing and applying WoEs (based on example 3)
@@ -56,7 +96,6 @@ train_woes <- data.frame(creditability = train_bins$creditability,
 valid_woes <- predict(woe_model, valid_bins)
 
 
-
 ### example 5: computing IVs  (based on example 4)
 library(klaR)
 woe_model <- woe(creditability ~ ., data = train_bins, zeroadj = 0.5)
@@ -64,11 +103,9 @@ woe_model <- woe(creditability ~ ., data = train_bins, zeroadj = 0.5)
 woe_model$IV
 
 
-
 ### example 6: population stability analysis for all variables
 library(creditR)
 SSI.calc.data(train_bins, valid_bins, "creditability")
-
 
 
 ### example 7: population stability analysis for a single variable
@@ -78,7 +115,6 @@ psi(train_bins$purpose_bin, valid_bins$purpose_bin)
 
 psi_purp <- psi(train_bins$purpose_bin, valid_bins$purpose_bin)
 psi_purp$table[,-c(2,4)]
-
 
 
 ### example 8: visualizing correlations (based on example 4)
@@ -97,7 +133,6 @@ vctree  <- hclustvar(X.quanti = X[,-(1:2)])
 plot(as.phylo(vctree), type = "fan")
 
 
-
 ### example 9: variable clustering using ClustVarLV
 library(ClustVarLV)
 clverg <- CLV(train_woes[,-(1:2)], method = 1)
@@ -105,8 +140,20 @@ plot(clverg)
 summary(clverg, K = 3)  
 
 
+### example 10: Automatic correlation-based variable selection (based on example 4)
+library(creditmodel)
+# create list of variables sorted according to IV 
+iv_list = feature_selector(dat_train = train_woes, dat_test = NULL, 
+                           target = "creditability",
+                           filter = "IV", iv_cp = 0.02, vars_name = FALSE)
+iv_list
 
-### example 10: Cramer's V based variable selection  (based on example 3)
+# select variables
+fast_high_cor_filter(dat = train_woes, com_list = iv_list, p = 0.15, 
+                     cor_class = TRUE ,vars_name = TRUE)
+
+
+### example 11: Cramer's V based variable selection  (based on example 3)
 library(scorecardModelUtils)
 # package requires 0/1 target:
 train_bins2 <- train_bins
@@ -119,35 +166,29 @@ selection <- cv_filter(cvtable$cv_val_tab, ivtable$iv_table, threshold = 0.3)
 selection
 
 
-
-### example 11: BIC variable selection (based on example 4)
+### example 12: BIC variable selection (based on example 4)
 # column 2 (variable foreign.worker_bin) excluded as binned variable has only one level  
 null <- glm(creditability ~ 1, data = train_woes[,-2], family = binomial)
 full <- glm(creditability ~ ., data = train_woes[,-2], family = binomial)
 bicglm <- step(null, scope=formula(full), direction="both", k=log(nrow(train_woes)))
 
 
-
-### example 12: VIF (based on example 11)
+### example 13: VIF (based on example 11)
 car::vif(bicglm)
 scorecard::vif(bicglm)
 creditR::vif.calc(bicglm)
+creditmodel::lr_vif(bicglm)
 
 
-
-### example 13: calculation of scores (based on example 2)
-sc2    <- scorecard2(bins = bins, dt = train, y = 'creditability', x = names(train)[1:19])
+### example 14: calculation of scores (based on example 2)
+sc    <- scorecard2(bins = bins, dt = train, y = 'creditability', x = names(train)[1:19])
 # note: variable 20 (foreign.worker) not used (cf. also example 11)
-sc2
-train_scored <- scorecard_ply(train, sc2, only_total_score = FALSE)
-valid_scored <- scorecard_ply(valid, sc2, only_total_score = FALSE)
+sc
+sc$duration.in.month[,c(1,2,4,5,6,7,13)]
+sc$purpose[,c(1,2,4,5,6,7,13)]
 
-
-
-### example 14: output from example 13 for variables 'duration.in.month' and 'purpose'
-sc2$duration.in.month[,c(1,2,4,5,6,7,13)]
-sc2$purpose[,c(1,2,4,5,6,7,13)]
-
+train_scored <- scorecard_ply(train, sc, only_total_score = FALSE)
+valid_scored <- scorecard_ply(valid, sc, only_total_score = FALSE)
 
 
 ### example 15: scorecard points for model based on bins, not WoEs (based on example 4)
@@ -155,15 +196,20 @@ library(scorecardModelUtils)
 # create glm using factor variables -- foreign worker excluded (cf. above) 
 full_bins <- glm(creditability ~ ., data = train_bins[,-21], family = binomial)
 # calculate scorecard points from effects
-sc3 <- scalling(train_bins, "creditability", model = full_bins, point = 15, factor = 2)	
-sc3
+sc2 <- scalling(train_bins, "creditability", model = full_bins, point = 15, factor = 2)	
+sc2
 
 # apply scorecard to new data
-scoring(valid_bins, target = "creditability", sc3)
+scoring(valid_bins, target = "creditability", sc2)
 
 
+### example 16: directly predict score points from glm object (based on example 12)
+library(creditmodel)
+train_scored_3 <- score_transfer(bicglm, train_woes, a = 500, b = 20)
+valid_scored_3 <- score_transfer(bicglm, valid_woes, a = 500, b = 20)
 
-### example 16: Gini coefficient using {pROC} (based on example 13)
+
+### example 17: Gini coefficient using {pROC} (based on example 14)
 library(pROC)
 curve <- roc(valid$creditability, valid_scored$score, levels = c("good","bad"), direction = ">")
 # levels = c("controls", "cases"), 
@@ -175,15 +221,13 @@ auc(curve)
 ci(auc(curve), method = "bootstrap")
 
 
-
-### example 17: score bin frequencies (based on example 13) 
+### example 18: score bin frequencies (based on example 14) 
 library(scorecard)
 gt <- gains_table(valid_scored$score, valid$creditability, bin_num = 8)
 gt[,c(2,4,5,6,7,8,10,11,12)]
 
 
-
-### example 18: scorecard performance summary (based on example 13)
+### example 19: scorecard performance summary (based on example 13)
 library(smbinning)
 perf_dat <- data.frame("creditability" = as.integer(valid$creditability == "good"), 
                        "score" = valid_scored$score)
@@ -194,8 +238,7 @@ library(riskr)
 gg_perf(as.integer(valid$creditability == "good"), valid_scored$score)
 
 
-
-### example 19: Rating calibration (based on example 13)
+### example 20: Rating calibration (based on example 14)
 library(creditR)
 # calculate PDs from scores
 odds <- 1/19 * 2^(-(valid_scored$score - 600)/50)
@@ -214,8 +257,7 @@ bintest <- Binomial.test(mscale, "Total.Observations", "PD", "Bad.Rate")
 bintest[,c(1,3,4,5,8,9,14)]
 
 
-
-### example 20: reject inference using parcelling (based on example 4) 
+### example 21: reject inference using parcelling (based on example 4) 
 library(scoringTools)
 # use validation data as 'rejects' for this example
 # ...remove target variable and constant variable foreign.worker_bin
--- supplementary_code.R
+++ supplementary_code.R
@@ -1,10 +1,41 @@
-# Accompanying code to the 2019 version of paper.
-# A container with all packages in order to run the examples2019.R and the supplementary_code2019.R 
-# is available at docker hub and can be accessed via: docker run -p 8787:8787 -e PASSWORD=pwd grho/cswr.
+# > sessionInfo()
+# R version 4.1.2 (2021-11-01)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19042)
+# 
+# Matrix products: default
+# 
+# locale:
+# [1] LC_COLLATE=German_Germany.1252  LC_CTYPE=German_Germany.1252    LC_MONETARY=German_Germany.1252 LC_NUMERIC=C                   
+# [5] LC_TIME=German_Germany.1252    
+# 
+# attached base packages:
+# [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+# [1] klaR_0.6-15                 MASS_7.3-54                 woeBinning_0.1.6            scorecardModelUtils_0.0.1.0 smbinning_0.9              
+# [6] Formula_1.2-4               partykit_1.2-15             mvtnorm_1.1-3               libcoin_1.0-9               sqldf_0.4-11               
+# [11] RSQLite_2.2.9               gsubfn_0.7                  proto_1.0.0                 riskr_1.0                   scorecard_0.3.6            
+# 
+# loaded via a namespace (and not attached):
+# [1] lubridate_1.8.0     bit64_4.0.5         doParallel_1.0.16   tools_4.1.2         utf8_1.2.2          R6_2.5.1            rpart_4.1-15       
+# [8] DBI_1.1.2           questionr_0.7.5     colorspace_2.0-2    gbm_2.1.8           tidyselect_1.1.1    gridExtra_2.3       chron_2.3-56       
+# [15] bit_4.0.4           compiler_4.1.2      glmnet_4.1-3        cli_3.1.0           scales_1.1.1        randomForest_4.6-14 proxy_0.4-26       
+# [22] stringr_1.4.0       creditR_0.1.0       digest_0.6.29       rmarkdown_2.11      pkgconfig_2.0.3     htmltools_0.5.2     labelled_2.9.0     
+# [29] fastmap_1.1.0       highr_0.9           rlang_0.4.12        rstudioapi_0.13     MLmetrics_1.1.1     shiny_1.7.1         shape_1.4.6        
+# [36] generics_0.1.1      combinat_0.0-8      jsonlite_1.7.2      dplyr_1.0.7         zip_2.2.0           car_3.0-12          magrittr_2.0.1     
+# [43] Matrix_1.4-0        Rcpp_1.0.7          munsell_0.5.0       fansi_0.5.0         abind_1.4-5         lifecycle_1.0.1     stringi_1.7.6      
+# [50] yaml_2.2.1          inum_1.0-4          carData_3.0-4       plyr_1.8.6          blob_1.2.2          parallel_4.1.2      promises_1.2.0.1   
+# [57] forcats_0.5.1       crayon_1.4.2        miniUI_0.1.1.1      lattice_0.20-45     haven_2.4.3         splines_4.1.2       hms_1.1.1          
+# [64] knitr_1.37          pillar_1.6.4        tcltk_4.1.2         xgboost_1.5.0.2     reshape2_1.4.4      codetools_0.2-18    glue_1.6.0         
+# [71] evaluate_0.14       data.table_1.14.2   vctrs_0.3.8         httpuv_1.6.5        foreach_1.5.1       creditmodel_1.3.0   gtable_0.3.0       
+# [78] purrr_0.3.4         tidyr_1.1.4         assertthat_0.2.1    cachem_1.0.6        ggplot2_3.3.5       xfun_0.29           openxlsx_4.2.5     
+# [85] mime_0.12           xtable_1.8-4        e1071_1.7-9         later_1.3.0         class_7.3-19        survival_3.2-13     tibble_3.1.6       
+# [92] iterators_1.0.13    memoise_2.0.1       cluster_2.1.2       ellipsis_0.3.2      ROCR_1.0-11       
+
 
 ####################
 ### Section Data ###
-
 
 ### snippet 1: load data
 library(scorecard)
@@ -23,7 +54,13 @@ train2$creditability <- as.integer(train2$creditability == "good")
 valid2$creditability <- as.integer(valid2$creditability == "good")
 names(train2) <- names(valid2) <- gsub("\\.", "_", names(train2))
 
-
+# the package creditmodel does not allow variables of type Factor but character instead.
+train3 <- as.data.frame(train2)
+valid3 <- as.data.frame(valid2)
+for (j in which(sapply(train3[,-21], is.factor))) {
+  train3[,j] <- as.character(train3[,j])
+  valid3[,j] <- as.character(valid3[,j])
+}
 
 
 ################################
@@ -33,7 +70,7 @@ names(train2) <- names(valid2) <- gsub("\\.", "_", names(train2))
 library(riskr)
 
 # automatic binning of variable purpose
-bins_purpose_riskr <- superv_bin(train2$purpose, train2$creditability, 
+bins_purpose_riskr <- superv_bin(tibble::as_tibble(train2)$purpose, tibble::as_tibble(train2)$creditability, 
                                  min.p = 0.05, min.cri = 0.95, max.depth = 5)
 
 # element where the tree model is stored
@@ -43,7 +80,7 @@ bins_purpose_riskr$tree
 head(bins_purpose_riskr$data)
 
 # summary for binning of all variables 
-ez_summ_biv(train2, "creditability")
+ez_summ_biv(tibble::as_tibble(train2), "creditability")
 
 # assign bins to new data. 
 # ...first, helper variable with name 'variable has to be created'
@@ -53,8 +90,6 @@ valid2_bins$purpose_bin  <- as.factor(predict(bins_purpose_riskr$tree, valid2_bi
 valid2_bins$variable     <- NULL # remove helper variable
 # no assignment of WoEs possible, for WoEs add e.g. subsequent package {klaR} 
 table(valid2_bins$purpose, valid2_bins$purpose_bin)
-
-
 
 
 ### snippet 3: store binning of factor variable in lookup table and apply it to data set
@@ -84,38 +119,34 @@ train_bins$purpose_binned <- as.factor(as.character(mapping[as.character(train_b
 table(train_bins$purpose, train_bins$purpose_binned)
 
 
-
-
 ### snippet 4: Example of looping through all (numeric) variables for package {smbinning} 
 library(smbinning)
 
 # additional preprocessing as no points in variable names allowed... 
-train3 <- train2; valid3 <- valid2
-names(train3) <- names(valid3) <- gsub("\\.", "_", names(train3))
+train4 <- train2; valid4 <- valid2
+names(train4) <- names(valid4) <- gsub("\\.", "_", names(train4))
 
 # identify names of all numeric variables
-numerics <- names(train3)[sapply(train3, is.numeric)]
+numerics <- names(train4)[sapply(train4, is.numeric)]
 
 # loop binning through all numeric variables 
-bins2_list  <- lapply(numerics, function(z) smbinning(train3, y = "creditability", x = z))
+bins2_list  <- lapply(numerics, function(z) smbinning(train4, y = "creditability", x = z))
 
 # apply binning to training and validation data for all numeric variables 
 for(i in seq(along.with = bins2_list)){
   varname <- paste(numerics[i],"bin",sep="_")
   # check for each variabe whether a binning has been generated (i.e. at least 5 distinct values) 
   if(is.list(bins2_list[[i]])){
-    train3 <- smbinning.gen(train3, bins2_list[[i]], varname)
-    valid3 <- smbinning.gen(valid3, bins2_list[[i]], varname)
+    train4 <- smbinning.gen(train4, bins2_list[[i]], varname)
+    valid4 <- smbinning.gen(valid4, bins2_list[[i]], varname)
   }
   else{
-    train3[[varname]] <- as.factor(train3[[numerics[i]]])
-    valid3[[varname]] <- as.factor(valid3[[numerics[i]]])
+    train4[[varname]] <- as.factor(train4[[numerics[i]]])
+    valid4[[varname]] <- as.factor(valid4[[numerics[i]]])
   }
 } 
 
-str(train3) # note: original variables still available
-
-
+str(train4) # note: original variables still available
 
 
 ### snippet 5: group levels with low frequencies together
@@ -135,19 +166,17 @@ head(mapping$cat_class_new)
 mapping$cat_class_new[mapping$cat_class_new$Variable_name == "purpose" ,]
 
 # script to apply mapping to new data
-train4 <- mapping$base_new  # training data is already mapped
-valid4 <- valid2
+train5 <- mapping$base_new  # training data is already mapped
+valid5 <- valid2
 for(i in 1:nrow(mapping$cat_class_new)){
   vname <- mapping$cat_class_new$Variable_name[i]
-  x <- as.character(valid4[[vname]])
+  x <- as.character(valid5[[vname]])
   x[x == mapping$cat_class_new$old_class[i]] <- mapping$cat_class_new$new_class[i]
-  valid4[[vname]] <- x
+  valid5[[vname]] <- x
 }
 
 mapping$cat_class_new[mapping$cat_class_new$Variable_name == "purpose" ,]
-table(valid2$purpose, valid4$purpose)
-
-
+table(valid2$purpose, valid5$purpose)
 
 
 ### snippet 6:
@@ -160,8 +189,6 @@ defrates[order(defrates[,1]),]
 # ...comparison with bins 
 bins_purpose 
 #...bins are merged alphabetically
-
-
 
 
 ### snippet 7: function to transform results from woeBinning::woe.binning() ... 
@@ -204,8 +231,6 @@ wb     <- woeBins2breakslist(bins = bins_woeb, data = train)
 bins_imported <- scorecard::woebin(train, y = "creditability", breaks_list = wb)
 
 
-
-
 ### snippet 8: assign WoEs
 library(scorecard)
 bins         <- woebin(train, y = "creditability")
@@ -216,16 +241,6 @@ library(woeBinning)
 bins       <- woe.binning(as.data.frame(train), "creditability", as.data.frame(train), event.class = "bad")
 train_woes <- woe.binning.deploy(train, bins, add.woe.or.dum.var = "woe")
 str(train_woes) # returns original, binned and woes
-
-library(woeR)
-# use train3 (cf. snippet 4) -- variable names must not contain '.'s
-bins_dur <- woe_binning(train3, "duration_in_month", dv = "creditability", 
-                        min_perc = 0.02, initial_bins = 50, woe_cutoff = 0.1)
-
-train_woe_dur <- apply_woe(train3, bins_dur)
-str(train_woe_dur) # original data frame plus additional woe column for duration 
-
-
 
 
 #######################
@@ -265,15 +280,13 @@ sc <- scorecard(renamed_bins, bicglm, points0 = 600, odds0 = 1/19, pdo = 50, bas
 sc
 
 
-
-
 ### snippet 10: (re-)compute scorecard using scorecard2() from an existing glm object 
-###             ...based on example 11 (bicglm) 
+###             ...based on example 12 (bicglm) 
 
 # create vector of input variable names in original data set from existing glm object (on woe variables) 
 orig.names <- names(coef(bicglm))[-1] # exclude intercept
 orig.names <- sub("_bin", "", orig.names)
 orig.names <- sub("woe_", "", orig.names)
 
-sc    <- scorecard2(bins = bins, dt = train, y = 'creditability', x = orig.names)
-sc
+sc2    <- scorecard2(bins = bins, dt = train, y = 'creditability', x = orig.names)
+sc2
